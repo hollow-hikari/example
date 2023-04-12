b65f27ab4d875ba3cc036c50fdfb7dec53cd89be
Prepod_Admin <Prepod_Admin@DESKTOP-UN3GB9T>
2023-04-12 20:31:54 +0300

    Добавили функцию возведения в квадрат

--- ConsoleApplication8.cpp
+++ ConsoleApplication8.cpp
@@ -1,9 +1,15 @@
 ﻿#include <iostream>
 using namespace std;
 
+int square(int x) {
+    return x * x;
+}
+
 int main()
 {
     cout << "Hello World!\n";
     cout << "Another line!";
     cout << "\nThis is new branch!";
+
+    cout << square(5);
 }
